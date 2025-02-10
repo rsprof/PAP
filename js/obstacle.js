cf3ded43af483fe353c455eea9b7bc92a5c55250
rsprof <98306718+rsprof@users.noreply.github.com>
2025-02-10 15:08:39 +0000

    Game almost finished

--- js/obstacle.js
+++ js/obstacle.js
@@ -1,24 +1,34 @@
 class Obstacle  {
     constructor(game, x)   {
         this.game = game;
-        this.spriteWidth = 110;
-        this.spriteHeight = 150;
+        this.spriteWidth = 80;
+        this.spriteHeight = 110;
         this.scaledWidth = this.spriteWidth * this.game.ratio;
         this.scaledHeight = this.spriteHeight * this.game.ratio;
         this.x = x;
         this.y = Math.random() * (this.game.height - this.scaledHeight);
         this.speedY = Math.random() < 0.5 ? -1 * this.game.ratio : 1 * this.game.ratio;
         this.markedForDeletion = false;
-        this.collisionX;  //Collision
-        this.collisionY;   //Collision
-        this.collisionRadius = this.scaledWidth * 0.5;   //Collision
+        this.collisionX;
+        this.collisionY;
+        this.collisionRadius = this.scaledWidth * 0.5;
+        this.image = document.getElementById('obstacle'); //Obstacle Spritesheet
+        this.frameY = 0; //Obstacle Spritesheet
+        this.numberFrames = 18; //Obstacle Spritesheet
     }
 
-    update()    {
+    update()    { 
+        this.frameY = this.frameY % this.numberFrames; //Obstacle Spritesheet
+
         this.x -= this.game.speed;
         this.y += this.speedY;
-        if(this.y <= 0 || this.y >= this.game.height - this.scaledHeight)   {
-            this.speedY *= -1;    
+
+        if(!this.game.gameOver)  {
+            if(this.y <= 0 || this.y >= this.game.height - this.scaledHeight)   {
+                this.speedY *= -1;
+            }
+        } else  {
+            this.speedY += 0.1;
         }
 
         if(this.isOffScreen())  {
@@ -28,20 +38,26 @@ class Obstacle  {
             if(this.game.obstacles.length <= 0) this.game.gameOver = true;
         }
 
-        this.collisionX = this.x + this.scaledWidth * 0.5;  //Collision
-        this.collisionY= this.y + this.scaledHeight * 0.5;   //Collision
+        this.collisionX = this.x + this.scaledWidth * 0.5;
+        this.collisionY= this.y + this.scaledHeight * 0.5;
 
-        if(this.game.checkCollision(this, this.game.player))    {   //Collision
-            this.game.gameOver = true;   //Collision
-            this.game.player.collided = true;   //Collision
-        }   //Collision
+        if(this.game.checkCollision(this, this.game.player))    {
+            this.game.gameOver = true;
+            this.game.player.collided = true;
+            this.game.player.stopCharge();
+        }
     }
 
     draw()  {
-        this.game.ctx.fillRect(this.x, this.y, this.scaledWidth, this.scaledHeight);
-        this.game.ctx.beginPath();   //Collision
-        this.game.ctx.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);   //Collision
-        this.game.ctx.stroke();   //Collision
+        this.game.ctx.drawImage(this.image, 0, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.scaledWidth, this.scaledHeight); //Obstacle Spritesheet
+       
+        ++this.frameY;
+        
+        if(this.game.debug) { // Debug
+            this.game.ctx.beginPath();
+            this.game.ctx.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+            this.game.ctx.stroke();
+        } // Debug
     }
 
     resize()    {
@@ -50,6 +66,6 @@ class Obstacle  {
     }
 
     isOffScreen()   {
-        return this.x < -this.scaledWidth;
+        return this.x < -this.scaledWidth || this.y > this.game.height;
     }
 }
--- js/player.js
+++ js/player.js
@@ -3,36 +3,56 @@ class Player {
         this.game = game;
         this.x = 20;
         this.y = 0;
-        this.spriteWidth = 200;
-        this.spriteHeight = 200;    
+        this.spriteWidth = 130;
+        this.spriteHeight = 121;    
         this.width;
         this.height;    
         this.speedY;
         this.flapSpeed;
-        this.collisionX;  //Collision
-        this.collisionY;   //Collision
-        this.collisionRadius;   //Collision
-        this.collided;   //Collision
+        this.collisionX;
+        this.collisionY;
+        this.collisionRadius;
+        this.collided;
+        this.energy = 30;
+        this.maxEnergy = this.energy * 2;
+        this.minEnergy = 15;
+        this.charging;
+        this.barSize;
+        this.image = document.getElementById('player_bird'); //Player Spritesheet
+        this.frameY; //Player Spritesheet
     }
 
     draw() {
-        this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
-        this.game.ctx.beginPath();   //Collision
-        this.game.ctx.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);   //Collision
-        this.game.ctx.stroke();   //Collision
+        this.game.ctx.drawImage(this.image, 0, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height); //Player Spritesheet
+        if(this.game.debug) { // Debug
+            this.game.ctx.beginPath();
+            this.game.ctx.arc(this.collisionX + this.collisionRadius * 0.5, this.collisionY + 18, this.collisionRadius, 0, Math.PI * 2);
+            this.game.ctx.stroke();       
+        } // Debug
     }
 
     update() {
+        this.handleEnergy();
+
+        if(this.speedY >= 0)    { //Player Spritesheet
+            this.wingsUp(); //Player Spritesheet
+         } //Player Spritesheet
+    
         this.y += this.speedY;
+
+        if(!this.isTouchingBottom() && !this.charging)    {
+            this.speedY += this.game.gravity; 
+        }
+        else    {
+            this.speedY = 0;
+        }
 
         if(this.isTouchingBottom())    {
             this.y = this.game.height - this.height;
-        }
-        else    {
-            this.speedY += this.game.gravity;
+            this.wingsIdle(); //Player Spritesheet
         }
 
-        this.collisionY = this.y + this.height * 0.5;   //Collision
+        this.collisionY = this.y + this.height * 0.5;
     }
 
     resize() {
@@ -40,10 +60,14 @@ class Player {
         this.height = this.spriteHeight * this.game.ratio;
         this.y = this.game.height * 0.5 - this.height * 0.5;
         this.speedY = -8 * this.game.ratio;
-        this.flapSpeed = 6 * this.game.ratio;
-        this.collisionX = this.x + this.width * 0.5;   //Collision
-        this.collisionRadius = this.width * 0.5;   //Collision
-        this.collided = false;   //Collision
+        this.flapSpeed = 5  * this.game.ratio;
+        this.collisionX = this.x + this.width * 0.5;
+        this.collisionRadius = 30 * this.game.ratio;
+        this.collided = false;
+        this.barSize = Math.floor(5 * this.game.ratio);
+        this.frameY = 0; //Player Spritesheet
+        this.charging = false; //Player Spritesheet
+        this.energy = 30; //Player Spritesheet
     }
 
     isTouchingBottom()  {
@@ -55,8 +79,61 @@ class Player {
     }
 
     flap()  {
+        this.stopCharge();
+
         if(!this.isTouchingTop())    {
             this.speedY = -this.flapSpeed;
+            this.wingsDown(); //Player Spritesheet
         }
     }
+
+    handleEnergy()  {
+        if(this.game.eventUpdate)   {
+            if(this.energy < this.maxEnergy)    {
+                this.energy += 1;
+            }
+
+            if(this.charging)    {
+                this.energy -= 6;
+
+                if(this.energy <= 0)    {
+                    this.energy = 0;
+                    this.stopCharge();
+                }
+            }
+        }
+    }
+
+    startCharge()   {
+        this.game.sound.play(this.game.sound.charge); // Sound
+        this.charging = true;
+        this.game.speed = this.game.maxSpeed;
+        this.wingsCharge(); //Player Spritesheet
+    }
+
+    stopCharge()    {
+        this.charging = false;
+        this.game.speed = this.game.minSpeed;
+        this.wingsIdle(); //Player Spritesheet
+    }
+
+    wingsIdle() { //Player Spritesheet
+        this.frameY = 0; //Player Spritesheet
+    } //Player Spritesheet
+
+    wingsDown() { //Player Spritesheet
+        if(!this.charging)    {
+           this.frameY = 1; //Player Spritesheet
+        }
+    } //Player Spritesheet
+
+    wingsUp() { //Player Spritesheet
+        if(!this.charging)    {
+            this.frameY = 2; //Player Spritesheet
+         }
+    } //Player Spritesheet
+
+    wingsCharge() { //Player Spritesheet
+        this.frameY = 3; //Player Spritesheet
+    } //Player Spritesheet
 }
